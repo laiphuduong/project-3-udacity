7127a0e362399b7eb504c8cf55f17f14076915a8
duonglp <duonglp97@gmail.com>
2022-03-29 15:19:44 +0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-773678711130-test.credesnau36g.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=udagram
+export AWS_BUCKET=project3-773678711130-test
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=sing-profile
+export JWT_SECRET=testing
+export URL=http://localhost:8100
